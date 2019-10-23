51a14ca511a5403c05eaf3caf32f6e8ef68c66ff
Shruti Gaonkar <shruti.korgaonkar@gmail.com>
2019-10-23 01:34:12 -0700

    Guessing word logic and decrementing guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,28 @@
+var win = 0;
 var game = {
-    wordArr : [],
-    word: '',
-    noOfLettersInWord : 0,
-    guessesLeft : 0,
-    guessedLettersArr : [],
-    spacePositionArr : [],
-    wins : 0,
-    initialiseWordData : function(){
+    initialiseData: function(){
+        this.wordArr = [];
+        this.word = '';
+        this.noOfLettersInWord  = 0;
+        this.guessesLeft = 0;
+        this.match_point = 0;
+        this.guessedLettersArr = [];
+        this.spacePositionArr = [];
+    },
+    getWordAndBlanks : function(){
         this.wordArr.push("Cinderella", "Rupentzel", "Beauty and the Beast");
 
+        /* get the word */
         this.word = this.wordArr[Math.floor(Math.random() * this.wordArr.length)];
         this.word = this.word.toLowerCase();
 
+        /* get no of letters in a word */
         this.noOfLettersInWord = this.word.length;
         
+        /* get no of guesses left */
         this.guessesLeft = this.word.length * 2;
-
+        
+        /* to display the blanks before word is guessed including spaces*/
         if(this.noOfLettersInWord>0){
             spacePosition = this.word.indexOf(' ');
             while (spacePosition !== -1) {
@@ -33,23 +40,40 @@ var game = {
         }
     },
     guessWord: function(key){
-        
+        console.log(this.word);
+        var position = this.word.indexOf(key);
+        //console.log(position);
+        while (position !== -1) {
+            this.guessedLettersArr.splice(position, 1, key);
+            position = this.word.indexOf(key, position + 1);
+        }
+        if(this.guessedLettersArr.includes("_")===false){
+            this.match_point++;
+        }else{
+            game.guessesLeft--;
+        }
     }
 }
-
-
 
 
 document.onkeyup = function(){
     var letter = event.key.toLowerCase();
-    if(letter && !game.word){
-        game.initialiseWordData();
+    if((letter && !game.word) || game.guessesLeft==0){
+        game.initialiseData();
+        game.getWordAndBlanks();
+    }else if(game.guessesLeft>0 && letter!=' '){
+        game.guessWord(letter);
     }
 
-
+    if(game.match_point>0){
+        game.initialiseData();
+        game.getWordAndBlanks();
+        win++;
+        if(win>0)
+        document.getElementById('win_id').textContent = win;
+    }
     var guessedLetters = game.guessedLettersArr.toString();
-    //guessedLetters = guessedLetters.replace(/ /g, '   ');
     guessedLetters = guessedLetters.replace(/,/g, ' ');
     document.getElementById('word_id').textContent    = guessedLetters;
+    document.getElementById('guesses_id').textContent = game.guessesLeft + " guesses left";
 }
-
